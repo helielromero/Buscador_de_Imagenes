7cf4a6b0b24d17edfb2c916b13c35e2a1a82ad1d
helielromero <84945710+helielromero@users.noreply.github.com>
2022-04-18 20:08:02 -0500

    se modifico splashScreen por SplashActivity

--- app/src/main/java/briix/com/buscadordeimagenes/SplashActivity.kt
+++ app/src/main/java/briix/com/buscadordeimagenes/SplashActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class SplashScreen : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val intent = Intent(this, OnboardingActivity::class.java)
